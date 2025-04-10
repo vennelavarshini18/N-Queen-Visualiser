ec0f74ab5cd70d7418edae0950f9dcf554014be8
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2025-04-10 11:14:43 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NQueen from './Compenents/Visualizer/nqueenComponent';
+
+function App() {
+	return (
+		<div className="App">
+			<NQueen />
+		</div>
+	);
+}
+
+export default App;
